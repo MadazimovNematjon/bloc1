b82d4ce0462b52f2893cdeb3bae9eeebfd84d1b1
MadazimovNematjon <118611615+MadazimovNematjon@users.noreply.github.com>
2023-07-24 09:05:09 +0500

    create Page

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -1,81 +1,78 @@
-import 'package:bloc1/bloc/create_post_cubit.dart'; // Import the CreatePostCubit
-import 'package:bloc1/bloc/create_post_state.dart'; // Import the CreatePostState
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CreateData extends StatefulWidget {
+import '../bloc/create_post_cubit.dart';
+import '../bloc/create_post_state.dart';
+
+// Import the CreatePostCubit, CreatePostState, and CreatePostEvent classes from the provided code
+
+class CreateData extends StatelessWidget {
   static String get id => "create_data";
 
   const CreateData({Key? key}) : super(key: key);
 
   @override
-  State<CreateData> createState() => _CreateDataState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Create Data")),
+      body: BlocProvider(
+        create: (_) => CreatePostCubit(),
+        child: CreatePostForm(),
+      ),
+    );
+  }
 }
 
-class _CreateDataState extends State<CreateData> {
+class CreatePostForm extends StatelessWidget {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _bodyController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Create Data")),
-      body: BlocBuilder<CreatePostCubit, CreatePostState>(
-        builder: (context, state) {
-          if (state is CreatePostError) {
-            return createPost(state.errorMessage); // Pass error message to createPost widget
-          }
-          // Handle other states here if needed, e.g., loading or success state
-          return Container(); // Return an empty container for now
-        },
-      ),
+    return BlocConsumer<CreatePostCubit, CreatePostState>(
+      listener: (context, state) {
+        if (state is CreatePostError) {
+          _showSnackBar(context, state.errorMessage);
+        } else if (state is CreatePostSuccess) {
+          _showSnackBar(context, "Post created successfully!");
+        }
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _titleController,
+                onChanged: (value) => context.read<CreatePostCubit>().onTitleChanged(value),
+                decoration: const InputDecoration(
+                  hintText: "Title",
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                controller: _bodyController,
+                onChanged: (value) => context.read<CreatePostCubit>().onBodyChanged(value),
+                decoration: const InputDecoration(
+                  hintText: "Body",
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => context.read<CreatePostCubit>().createPost(),
+                child: const Text("Create"),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 
-  Widget createPost(String errorMessage) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _titleController,
-            decoration: const InputDecoration(
-              hintText: "Title",
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          TextField(
-            controller: _bodyController,
-            decoration: const InputDecoration(
-              hintText: "Body",
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              _onCreateButtonPressed();
-            },
-            child: const Text("Create"),
-          ),
-          if (errorMessage.isNotEmpty) // Show error message if available
-            Text(
-              errorMessage,
-              style: TextStyle(color: Colors.red),
-            ),
-        ],
-      ),
+  void _showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
     );
-  }
-
-  void _onCreateButtonPressed() {
-    String title = _titleController.text.trim();
-    String body = _bodyController.text.trim();
-    CreatePostCubit createPostCubit = BlocProvider.of<CreatePostCubit>(context);
-    createPostCubit.onTitleChanged(title);
-    createPostCubit.onBodyChanged(body);
-    createPostCubit.createPost();
   }
 }
